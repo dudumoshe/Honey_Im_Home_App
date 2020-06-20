2b7cffb4d0983e1642b6de7f4f9afe0485bd5324
dudumoshe <moshe.dudu@lightricks.com>
2020-06-20 22:15:14 +0300

    test sms button working and saving to sp

--- app/src/main/java/com/example/honey_im_home_app/MainActivity.java
+++ app/src/main/java/com/example/honey_im_home_app/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import android.Manifest;
 import android.content.BroadcastReceiver;
@@ -14,9 +15,11 @@ import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
     private BroadcastReceiver locationBroadcastReciever;
     private LocationInfo lastGoodLocation = null;
     private static int REQUEST_LOCATION_PERMISSION = 3654;
+    private static int SEND_SMS_PERMISSION = 3612;
+    private static String SMS_CONTENT = "Honey I'm Sending a Test Message!";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +36,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setupSetHomeLocationButton();
         setupClearHomeLocationButton();
+        setupSetSMSPhoneButton();
+        setupTestSMSButton();
         sp = getSharedPreferences("HomeLocationData", MODE_PRIVATE);
         String homeLocation = sp.getString("homeLocation", null);
+        String phoneNumber = sp.getString("userPhoneNumber", null);
         onSetHomeLocation(homeLocation);
+        onSetPhoneNumber(phoneNumber);
         listenLocationChanges();
 
         findViewById(R.id.start_tracking_location).setOnClickListener(new View.OnClickListener() {
@@ -46,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
                     ActivityCompat.requestPermissions(MainActivity.this,
                             new String[]{ Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION_PERMISSION);
                 } else {
-                    Log.d("Main Activity","got permissions");
+                    Log.d("Main Activity","got permissions to get location");
                     trackerStartOrStop();
                 }
             }
@@ -69,30 +78,68 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            Log.d("Main Activity","got permissions");
-            trackerStartOrStop();
-        } else {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d("Main Activity","got permissions");
+                trackerStartOrStop();
+            } else if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.ACCESS_FINE_LOCATION)) {
                 new AlertDialog.Builder(this)
-                    .setTitle("Location Permission Importance")
-                    .setMessage("Location permission is important for using this application")
-                    .setPositiveButton("ok", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            //Prompt the user once explanation has been shown
-                            ActivityCompat.requestPermissions(MainActivity.this,
-                                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                                    REQUEST_LOCATION_PERMISSION);
-                        }
-                    })
-                    .create()
-                    .show();
+                        .setTitle("Location Permission Importance")
+                        .setMessage("Location permission is important for using this application")
+                        .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Prompt the user once explanation has been shown
+                                ActivityCompat.requestPermissions(MainActivity.this,
+                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                        REQUEST_LOCATION_PERMISSION);
+                            }
+                        })
+                        .create()
+                        .show();
+            }
+        } else if (requestCode == SEND_SMS_PERMISSION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d("Main Activity","got permissions");
+                getNewPhoneNumber();
+            } else if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.SEND_SMS)) {
+                new AlertDialog.Builder(this)
+                        .setTitle("Send SMS Permission Importance")
+                        .setMessage("SMS permission is important for using this application")
+                        .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Prompt the user once explanation has been shown
+                                ActivityCompat.requestPermissions(MainActivity.this,
+                                        new String[]{Manifest.permission.SEND_SMS},
+                                        SEND_SMS_PERMISSION);
+                            }
+                        })
+                        .create()
+                        .show();
             }
         }
     }
 
+    private void setupTestSMSButton() {
+        findViewById(R.id.test_sms).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phoneNumber = sp.getString("userPhoneNumber", null);
+                if (phoneNumber == null) {
+                    return;
+                }
+                Intent smsIntent = new Intent();
+                smsIntent.setAction(LocalSendSmsBroadcastReceiver.actionName);
+                smsIntent.putExtra(LocalSendSmsBroadcastReceiver.smsMessageContentKey, SMS_CONTENT);
+                smsIntent.putExtra(LocalSendSmsBroadcastReceiver.phoneNumberKey, phoneNumber);
+                MainActivity.this.sendBroadcast(smsIntent);
+                Log.d("Main Activity", "finished asking to send sms to " + phoneNumber);
+            }
+        });
+    }
     private void trackerStartOrStop() {
         LocationTracker locationTracker = ((HoneyImHomeApp)getApplicationContext()).locationTracker;
         Button turnTrackerOnButton = findViewById(R.id.start_tracking_location);
@@ -151,12 +198,67 @@ public class MainActivity extends AppCompatActivity {
         registerReceiver(this.locationBroadcastReciever, new IntentFilter("HoneyImHomeLocations"));
     }
 
+    private void setupSetSMSPhoneButton() {
+        findViewById(R.id.set_sms_phone_number).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean hasPermissions = ActivityCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED;
+
+                if(!hasPermissions) {
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{ Manifest.permission.SEND_SMS}, SEND_SMS_PERMISSION);
+                } else {
+                    Log.d("Main Activity","got permissions to send sms");
+                    getNewPhoneNumber();
+                }
+            }
+        });
+    }
+
+    private void getNewPhoneNumber() {
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_CLASS_PHONE);
+
+        new AlertDialog.Builder(this)
+                .setTitle("Please Insert Phone Number")
+                .setView(input)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String userPhone = input.getText().toString();
+                        if (userPhone.isEmpty()) {
+                            sp.edit().remove("userPhoneNumber").apply();
+                        } else {
+                            sp.edit().putString("userPhoneNumber", userPhone).apply();
+                        }
+                        onSetPhoneNumber(userPhone);
+                    }
+                }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                })
+                .create()
+                .show();
+    }
+
     private void onSetHomeLocation(String homeLocation) {
         if (homeLocation != null) {
             TextView homeLocationText = findViewById(R.id.home_location_text);
             homeLocationText.setText("Your home location is defined as " + homeLocation);
             Button clearHome = findViewById(R.id.clear_home);
             clearHome.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void onSetPhoneNumber(String phoneNumber) {
+        Button testSMS = findViewById(R.id.test_sms);
+        if (phoneNumber != null && !phoneNumber.isEmpty()) {
+            testSMS.setVisibility(View.VISIBLE);
+        } else {
+            testSMS.setVisibility(View.GONE);
         }
     }
 
